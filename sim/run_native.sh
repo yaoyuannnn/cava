a05a81cde88db7f04d5eb8ceb259fcb0309f4111
Saketh Rama <rama@seas.harvard.edu>
2018-10-19 20:48:38 -0400

    Edit sim to convert raw images for convenience.
    Still haven't found an nn conf but just letting that fail for now while
    I try to get the camera pipeline to look right.
    
    Currently, it seems to be turning a (480, 640, 3) PNG image of a tree
    into a speckled mostly-white image with some recognizable features from
    the original image, just with the wrong colors everywhere. Not sure if
    this is a bug in the camera pipeline implementation or a mistake in,
    say, the size or format of the inputs I'm providing.

--- sim/run_native.sh
+++ sim/run_native.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
+# Runs camera/vision pipeline on a target image file.
+# Converts input.png to result.png.
+
+input_image=input.png
+
 top_level=`git rev-parse --show-toplevel`
 bin_path=${top_level}/build/cam-vision-native
+load_and_convert=${top_level}/scripts/load_and_convert.py 
 
-${bin_path} raw_32x32.bin result.bin test.conf
+output_image_name=result
+raw_input_image=${input_image}
+binary_input_image=input.bin
+binary_output_image=${output_image_name}.bin
+
+# Convert raw image to binary.
+${load_and_convert} -r ${raw_input_image}
+
+# Run camera pipeline on binary, then DNN using specified configuration.
+${bin_path} ${binary_input_image} ${binary_output_image} test.conf
+
+# From binary back to raw, for user display.
+${load_and_convert} -b ${binary_output_image}
