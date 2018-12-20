e1920564c1773c55897bd927f3675dafc14a4e77
Yuan Yao <yaoyuannnn@gmail.com>
2018-12-20 11:05:06 -0500

    Create a branch for clean native build.
    The current native build still depends on a few files under the
    gem5-Aladdin repo. This commit copies all the required gem5 files
    to a local gem5 folder and removed all the gem5 include paths, making
    the native/debug builds independent of the external gem5-Aladdin repo.

---
+++ cam_vision_pipe/src/gem5/dma_interface.h
@@ -0,0 +1,44 @@
+#ifndef __DMA_INTERFACE_H__
+#define __DMA_INTERFACE_H__
+
+#include <stddef.h>
+
+#define PAGE_SIZE 4096
+
+#if defined(DMA_INTERFACE_V3)
+
+// Version 3 of the DMA interface enables memcpy operations from arbitrary
+// source and destination addresses.
+
+int dmaLoad(void* dst_addr, void* src_host_addr, size_t size);
+int dmaStore(void* dst_host_addr, void* src_addr, size_t size);
+
+// The user can explicitly toggle the state of ready bits, if ready mode is
+// enabled. This requires support from DMA v3.
+int setReadyBits(void* start_addr, size_t size, unsigned value);
+
+#elif defined(DMA_INTERFACE_V2)
+
+#warning "DMA interface v2 is deprecated!"
+
+// Version 2 of the DMA interface separates source and destination offsets from
+// the base address into different fields, and on the host machine, memory is
+// actually copied from source to destination (the memory copy will not show up
+// in the trace).
+
+void* dmaLoad(void* base_addr, size_t src_off, size_t dst_off, size_t size);
+void* dmaStore(void* base_addr, size_t src_off, size_t dst_off, size_t size);
+
+#else
+
+#warning "DMA interface v1 is deprecated!"
+
+// Version 1 of the DMA interface is now deprecated and will be removed entirely.
+
+void* dmaLoad(void* addr, size_t offset, size_t size);
+void* dmaStore(void* addr, size_t offset, size_t size);
+
+#endif
+void dmaFence();
+
+#endif
